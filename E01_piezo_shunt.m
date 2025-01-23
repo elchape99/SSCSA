0cf41ed0a6204b7e3e96ad86a4e2c321daed0da9
elchape99 <s4673275@studenti.unige.it>
2025-01-23 15:05:27 +0100

    pippo

--- E01_piezo_shunt.m
+++ E01_piezo_shunt.m
@@ -29,118 +29,126 @@ ne = round(L/le);
 a=le/2* ones(1,2*ne);
 ltot=2*a*ne;
 
+% find tau optimal
 wf = sqrt (w_i.^2 .* (1 + beam.k1.^2 ./ 2));
 beam.tau1 = 1 ./ wf;
 
+x = linspace(1,500, 6000);
+data = load ("FRF_sc_sc.mat");
+% plot the total FRF without control (sc, oc, only pezo 1)
+figure
+semilogy (x, data.Data1_MT_FRF_H1_2Zplus_1Zplus_Ampl)
 
 
-% discretize the lenght of the beam
-%x = (0:ltot(1)/ne:ltot(1)*(1-1/ne));
-%[erre,ci]=size(beam.PHI);
-%phi=zeros(60,6);
-
-for ii=1:ci
-    % discretize the magnitudo of mode shape PHI 
-    phi(:,ii)=beam.PHI(:,ii)./max(abs(beam.PHI(:,ii)));
-end
-
-% find the Frequency Respone Function (FRF)
-m=length(beam.PHI(:,1)); % measuring point, in our case equal to focing point
-omegav=2*pi*[0:0.01:nf(end)*1.3].'; % system excitation frequency !!!
-
-[Hsci, Hsc] = ComputeFRF (beam.PHI(:,1:4), beam.nf1_sc, omegav, beam.csi1, m, m);
-[Hoci, Hoc] = ComputeFRF (beam.PHI(:,1:4), beam.nf1_oc, omegav, beam.csi1, m, m);
-
-% plot all the staff
-plotting
-
-% tuning R and LR
-c0 = 33.26*10^-9; % measured capacity !!!!!
-% equivalent capacitance related at each mode(cpi into slides)
-cpt=[c0/(1+k(1)^2) c0/(1+k(1)^2)/(1+k(2)^2) c0/(1+k(1)^2)/(1+k(2)^2)/(1+k(3)^2) c0/(1+k(1)^2)/(1+k(2)^2)/(1+k(3)^2)/(1+k(4)^2)];
-rt=zeros(nmodes,1);
-
-for jj = 1:nmodes
-    Hr=zeros(length(omegav),1);
-    Hlr=zeros(length(omegav),1);
-    omf=sqrt((w_oc(jj)^2+w_i(jj)^2)/2);
-    ta=1/omf;
-    rt(jj)=ta/cpt(jj);
-    omet(jj)=w_oc(jj);
-    lt(jj)=1/(omet(jj)^2*cpt(jj));
-    xiet(jj)=sqrt(3)/2*sqrt((w_oc(jj)^2-w_i(jj)^2)/(w_oc(jj)^2+w_i(jj)^2));
-    rlt(jj)=2*xiet(jj)*sqrt(lt(jj)/cpt(jj));
-    for ii=1:nmodes
-        r=rt(jj);
-        l=lt(jj);
-        cpi=cpt(ii);
-        tau=r*cpi;
-        ome=sqrt(1/(l*cpi));
-        xie=rlt(jj)/2*sqrt(cpi/l);
-        csi=csit(ii);
-        omz=w_i(ii);
-        omoc=w_oc(ii);
-        fi_m=beam.PHI(m,ii);
-        fi_f=beam.PHI(m,ii);
-        % !!!! chiedere questa formul aalla prof perche è diversa dalla
-        % teoria , in georia non ce fi_m, fi_f
-        Hri=fi_m*fi_f*((1+1i*omegav*tau)./(-omegav.^2.*(1+2*csi*omz*tau)+1i*omegav.*(2*csi.*omz+omoc.^2*tau-tau*omegav.^2)+omz^2)); 
-        Hrli=fi_m*fi_f*(-omegav.^2+ome^2+2*1i*xie*ome*omegav)./(omegav.^4-omegav.^2.*(ome^2+4*csi*xie*omz*ome+omoc^2)+omz^2*ome^2+1i*omegav.*(2*xie*ome*(omoc^2-omegav.^2)+2*csi*omz*(ome^2-omegav.^2)));
-        Hr=Hri+Hr;
-        Hlr=Hrli+Hlr;
-    end
-    figure
-    ax1=subplot(2,1,1)
-    semilogy(omegav/2/pi,abs(Hsc),'--')
-    hold on
-    semilogy(omegav/2/pi,abs(Hoc),'--')
-    semilogy(omegav/2/pi,abs(Hr))
-    semilogy(omegav/2/pi,abs(Hlr))
-
-
-    xlabel('Frequency [Hz]')
-    ylabel('|H| [m/N]')
-    grid on
-    axis tight
-    legend('SC','OC','R','RL')
-
-    ax2=subplot(2,1,2)
-    plot(omegav/2/pi,angle(Hsc),'--')
-    hold on
-    plot(omegav/2/pi,angle(Hoc),'--')
-    plot(omegav/2/pi,angle(Hr))
-    plot(omegav/2/pi,angle(Hlr))
-
-    grid on
-    axis tight
-    xlabel('Frequency [Hz]')
-    ylabel('\phi [rad]')
-
-    linkaxes([ax1,ax2],'x');
-
-
-    % figure
-    % subplot(2,1,1)
-    % plot(omegav/2/pi,abs(Hsc),'--')
-    % hold on
-    % plot(omegav/2/pi,abs(Hoc),'--')
-    % plot(omegav/2/pi,abs(Hr))
-    % plot(omegav/2/pi,abs(Hlr))
-    % xlabel('Frequency [Hz]')
-    % ylabel('|H| [m/N]')
-    % grid on
-    % axis tight
-    % legend('SC','OC','R','RL')
-    % subplot(2,1,2)
-    % plot(omegav/2/pi,angle(Hsc),'--')
-    % hold on
-    % plot(omegav/2/pi,angle(Hoc),'--')
-    % plot(omegav/2/pi,angle(Hr))
-    % plot(omegav/2/pi,angle(Hlr))
-    % grid on
-    % axis tight
-    % xlabel('Frequency [Hz]')
-    % ylabel('\phi [rad]')
-    % clear Hr Hlr
-end
-
+% 
+% 
+% % discretize the lenght of the beam
+% %x = (0:ltot(1)/ne:ltot(1)*(1-1/ne));
+% %[erre,ci]=size(beam.PHI);
+% %phi=zeros(60,6);
+% 
+% for ii=1:ci
+%     % discretize the magnitudo of mode shape PHI 
+%     phi(:,ii)=beam.PHI(:,ii)./max(abs(beam.PHI(:,ii)));
+% end
+% 
+% % find the Frequency Respone Function (FRF)
+% m=length(beam.PHI(:,1)); % measuring point, in our case equal to focing point
+% omegav=2*pi*[0:0.01:nf(end)*1.3].'; % system excitation frequency !!!
+% 
+% [Hsci, Hsc] = ComputeFRF (beam.PHI(:,1:4), beam.nf1_sc, omegav, beam.csi1, m, m);
+% [Hoci, Hoc] = ComputeFRF (beam.PHI(:,1:4), beam.nf1_oc, omegav, beam.csi1, m, m);
+% 
+% % plot all the staff
+% plotting
+% 
+% % tuning R and LR
+% c0 = 33.26*10^-9; % measured capacity !!!!!
+% % equivalent capacitance related at each mode(cpi into slides)
+% cpt=[c0/(1+k(1)^2) c0/(1+k(1)^2)/(1+k(2)^2) c0/(1+k(1)^2)/(1+k(2)^2)/(1+k(3)^2) c0/(1+k(1)^2)/(1+k(2)^2)/(1+k(3)^2)/(1+k(4)^2)];
+% rt=zeros(nmodes,1);
+% 
+% for jj = 1:nmodes
+%     Hr=zeros(length(omegav),1);
+%     Hlr=zeros(length(omegav),1);
+%     omf=sqrt((w_oc(jj)^2+w_i(jj)^2)/2);
+%     ta=1/omf;
+%     rt(jj)=ta/cpt(jj);
+%     omet(jj)=w_oc(jj);
+%     lt(jj)=1/(omet(jj)^2*cpt(jj));
+%     xiet(jj)=sqrt(3)/2*sqrt((w_oc(jj)^2-w_i(jj)^2)/(w_oc(jj)^2+w_i(jj)^2));
+%     rlt(jj)=2*xiet(jj)*sqrt(lt(jj)/cpt(jj));
+%     for ii=1:nmodes
+%         r=rt(jj);
+%         l=lt(jj);
+%         cpi=cpt(ii);
+%         tau=r*cpi;
+%         ome=sqrt(1/(l*cpi));
+%         xie=rlt(jj)/2*sqrt(cpi/l);
+%         csi=csit(ii);
+%         omz=w_i(ii);
+%         omoc=w_oc(ii);
+%         fi_m=beam.PHI(m,ii);
+%         fi_f=beam.PHI(m,ii);
+%         % !!!! chiedere questa formul aalla prof perche è diversa dalla
+%         % teoria , in georia non ce fi_m, fi_f
+%         Hri=fi_m*fi_f*((1+1i*omegav*tau)./(-omegav.^2.*(1+2*csi*omz*tau)+1i*omegav.*(2*csi.*omz+omoc.^2*tau-tau*omegav.^2)+omz^2)); 
+%         Hrli=fi_m*fi_f*(-omegav.^2+ome^2+2*1i*xie*ome*omegav)./(omegav.^4-omegav.^2.*(ome^2+4*csi*xie*omz*ome+omoc^2)+omz^2*ome^2+1i*omegav.*(2*xie*ome*(omoc^2-omegav.^2)+2*csi*omz*(ome^2-omegav.^2)));
+%         Hr=Hri+Hr;
+%         Hlr=Hrli+Hlr;
+%     end
+%     figure
+%     ax1=subplot(2,1,1)
+%     semilogy(omegav/2/pi,abs(Hsc),'--')
+%     hold on
+%     semilogy(omegav/2/pi,abs(Hoc),'--')
+%     semilogy(omegav/2/pi,abs(Hr))
+%     semilogy(omegav/2/pi,abs(Hlr))
+% 
+% 
+%     xlabel('Frequency [Hz]')
+%     ylabel('|H| [m/N]')
+%     grid on
+%     axis tight
+%     legend('SC','OC','R','RL')
+% 
+%     ax2=subplot(2,1,2)
+%     plot(omegav/2/pi,angle(Hsc),'--')
+%     hold on
+%     plot(omegav/2/pi,angle(Hoc),'--')
+%     plot(omegav/2/pi,angle(Hr))
+%     plot(omegav/2/pi,angle(Hlr))
+% 
+%     grid on
+%     axis tight
+%     xlabel('Frequency [Hz]')
+%     ylabel('\phi [rad]')
+% 
+%     linkaxes([ax1,ax2],'x');
+% 
+% 
+%     % figure
+%     % subplot(2,1,1)
+%     % plot(omegav/2/pi,abs(Hsc),'--')
+%     % hold on
+%     % plot(omegav/2/pi,abs(Hoc),'--')
+%     % plot(omegav/2/pi,abs(Hr))
+%     % plot(omegav/2/pi,abs(Hlr))
+%     % xlabel('Frequency [Hz]')
+%     % ylabel('|H| [m/N]')
+%     % grid on
+%     % axis tight
+%     % legend('SC','OC','R','RL')
+%     % subplot(2,1,2)
+%     % plot(omegav/2/pi,angle(Hsc),'--')
+%     % hold on
+%     % plot(omegav/2/pi,angle(Hoc),'--')
+%     % plot(omegav/2/pi,angle(Hr))
+%     % plot(omegav/2/pi,angle(Hlr))
+%     % grid on
+%     % axis tight
+%     % xlabel('Frequency [Hz]')
+%     % ylabel('\phi [rad]')
+%     % clear Hr Hlr
+% end
+% 
